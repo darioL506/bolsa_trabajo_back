44efa3bb2817fae0825e268c53343b3523a2f617
Luis <info@luisquesadadesign.com>
2021-01-22 13:19:25 +0100

    Instalación Passport

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request) {
+
+        if (User::where('email', $request->get('email'))->count() == 1) {
+            return response()->json(['message' => 'Registro incorrecto. Revise las credenciales.', 'code' => 400], 400);
+        }
+        
+        $validatedData = [
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => $request->get('password'),
+        ];
+
+        $validatedData['password'] = Hash::make($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response()->json(['message' => ['user' => $user, 'access_token' => $accessToken], 'code' => 201], 201);
+    }
+
+    public function login(Request $request) {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response()->json(['message' => 'Login incorrecto. Revise las credenciales.', 'code' => 400], 400);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response()->json(['message' => ['user' => auth()->user(), 'access_token' => $accessToken], 'code' => 200], 200);
+    }
+}
