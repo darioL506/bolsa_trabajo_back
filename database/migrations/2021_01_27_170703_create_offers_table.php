50d4d5c978f9cf8b4c82d258dfebb8a1cd6db869
Luis <info@luisquesadadesign.com>
2021-01-27 19:15:13 +0100

    Migration y seeder offer ok

---
+++ database/migrations/2021_01_27_170703_create_offers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('vacant');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->string('description', 200);
+            $table->timestamps();
+
+            // Faltaria porner el id de la empresa y el ciclo al que pertenece la oferta
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
