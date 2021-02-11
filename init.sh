06fcb95ded85a1adb39b4c2abb72230ce6ddc0b0
isra9shadow <isra9movil@hotmail.com>
2021-02-11 06:40:25 +0100

    fix docker

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 cp .env.example .env
 echo "export PATH="$HOME/.config/composer/vendor/bin:$PATH"" >> ~/.bashrc
-composer install --prefer-dist
+
+
 composer update
+
 php artisan key:generate
+
+chmod 777 -R /app
 
 php artisan migrate --seed
 php artisan passport:install --force
