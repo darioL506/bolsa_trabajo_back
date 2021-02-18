0687704d0d931b1c33bb69f3aee0700b3c91c2da
isra9shadow <isra9movil@hotmail.com>
2021-02-18 09:48:18 +0100

    actualizacion docker

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,7 @@ FROM php:7.4.14-fpm
 WORKDIR "/app"
 
 # Install selected extensions and other stuff
-RUN apt-get update \
-    && apt-get -y --no-install-recommends install \
-    && docker-php-ext-install mysqli pdo pdo_mysql  \
-    && apt-get update  \
-    && apt-get upgrade -y  \
-    && apt-get install -y git zip unzip \
-    && apt-get clean; rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/* \
-    && curl -sS https://getcomposer.org/installer | php \
-    && mv composer.phar /usr/local/bin/composer
-
+COPY . /app
 COPY ./init.sh /tmp/
 ENTRYPOINT ["sh","/tmp/init.sh"]
 
--- init.sh
+++ init.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-cp .env.example .env
-echo "export PATH="$HOME/.config/composer/vendor/bin:$PATH"" >> ~/.bashrc
 
+apt update
+
+apt-get -y --no-install-recommends install docker-php-ext-install mysqli pdo pdo_mysql
+apt-get update
+apt-get upgrade -y
+apt-get install -y git zip unzip
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+cp .env.example .env
+echo "export PATH="$HOME/.config/composer/vendor/bin:$PATH"" >>~/.bashrc
 
 composer update
-
-php artisan key:generate
-
 chmod 777 -R /app
+php artisan key:generate
 
 php artisan migrate --seed
 php artisan passport:install --force
