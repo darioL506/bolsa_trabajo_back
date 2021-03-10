af12f32f3a04b44ff56e14e24488c62e825409b0
isra9shadow <isra9movil@hotmail.com>
2021-03-10 10:19:25 +0100

    fix static

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -124,7 +124,7 @@ class StudentController extends Controller
         return response()->json(['code' => 200, $data], 200);
     }
 
-    public function getStudent($user_id)
+    public static function getStudent($user_id)
     {
         return $student = Student::where('user_id', $user_id)->first();
     }
